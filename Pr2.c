ba1611d3ad8205cfec788bcb6e4bb7ba383c1d21
AS1311 <99547651+AS1311@users.noreply.github.com>
2022-02-13 13:20:32 +0530

    Add files via upload

--- Pr2.c
+++ Pr2.c
@@ -6,7 +6,9 @@ printf("enter the numbers to find maximum:");
 scanf("%d%d",&num1,&num2);
 if(num1>num2)
 printf("%d is maximum",num1);
-else
+else if(num2>num1) 
 printf("%d is maximum",num2);
+else
+printf("%d and %d are equal", num1, num2); 
 return 0;
 } 
